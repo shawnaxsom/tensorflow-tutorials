b40cd1c484083c4a2866e22093485587b3b59687
Shawn Axsom <axs221@gmail.com>
2020-01-05 14:34:25 -0500

    Trying to train model getting error Feature alone is not in features dictionary

--- tensorflow-org/app.py
+++ tensorflow-org/app.py
@@ -44,7 +44,11 @@ print('--------------')
 print(print_pandas_df(test_file_path))
 
 
-raw_train_data = get_dataset(train_file_path)
+SELECT_COLUMNS = ['survived', 'age', 'n_siblings_spouses', 'parch', 'fare']
+DEFAULTS = [0, 0.0, 0.0, 0.0, 0.0]
+raw_train_data = get_dataset(train_file_path,
+                             select_columns=SELECT_COLUMNS,
+                             column_defaults=DEFAULTS)
 raw_test_data = get_dataset(test_file_path)
 
 print('--------------')
@@ -57,7 +61,99 @@ def show_batch(dataset):
             print("{:20s}: {}".format(key, value.numpy()))
 
 
+def pack(features, label):
+    return tf.stack(list(features.values()), axis=-1), label
+
+
 print('--------------')
 print('Showing batch:')
 print('--------------')
 show_batch(raw_train_data)
+
+
+print('--------------')
+print('Packed data:')
+print('--------------')
+
+packed_dataset = raw_train_data.map(pack)
+
+for features, labels in packed_dataset.take(1):
+    print(features.numpy())
+    print()
+    print(labels.numpy())
+
+
+def get_categorical_data():
+    CATEGORIES = {
+        'sex': ['male', 'female'],
+        'class': ['First', 'Second', 'Third'],
+        'deck': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+        'embark_town': ['Cherbourg', 'Southhampton', 'Queenstown'],
+        'alone': ['y', 'n']
+        }
+
+    categorical_columns = []
+    for feature, vocab in CATEGORIES.items():
+        cat_col = tf.feature_column.categorical_column_with_vocabulary_list(
+            key=feature, vocabulary_list=vocab)
+        categorical_columns.append(tf.feature_column.indicator_column(cat_col))
+
+    return categorical_columns
+
+
+class PackNumericFeatures(object):
+    def __init__(self, names):
+        self.names = names
+
+    def __call__(self, features, labels):
+        numeric_features = [features.pop(name) for name in self.names]
+        numeric_features = [tf.cast(feat, tf.float32) for feat in numeric_features]
+        numeric_features = tf.stack(numeric_features, axis=-1)
+        features['numeric'] = numeric_features
+
+        return features, labels
+
+
+NUMERIC_FEATURES = ['age', 'n_siblings_spouses', 'parch', 'fare']
+packed_train_data = raw_train_data.map(PackNumericFeatures(NUMERIC_FEATURES))
+packed_test_data = raw_test_data.map(PackNumericFeatures(NUMERIC_FEATURES))
+
+desc = pd.read_csv(train_file_path)[NUMERIC_FEATURES].describe()
+MEAN = np.array(desc.T['mean'])
+STD = np.array(desc.T['std'])
+
+
+def normalize_numeric_data(data, mean, std):
+    # Center the data
+    return (data-mean)/std
+
+
+def get_numeric_data():
+    normalizer = functools.partial(normalize_numeric_data, mean=MEAN, std=STD)
+    numeric_column = tf.feature_column.numeric_column('numeric', normalizer_fn=normalizer, shape=[len(NUMERIC_FEATURES)])
+    numeric_columns = [numeric_column]
+    return numeric_columns
+
+
+preprocessing_layer = tf.keras.layers.DenseFeatures(
+        get_categorical_data()
+        + get_numeric_data())
+
+
+model = tf.keras.Sequential([
+    preprocessing_layer,
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid'),
+])
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metris=['accuracy'])
+
+
+train_data = packed_train_data.shuffle(500)
+test_data = packed_test_data
+
+model.fit(train_data, epochs=20)
